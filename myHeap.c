b6023a5adc3d586552400e58dd6dc4fb5d2bfa0c
bofeiw <bofeiw@icloud.com>
2019-08-04 20:15:26 +1000

    implement initHeap

--- myHeap.c
+++ myHeap.c
@@ -19,25 +19,25 @@
 
 /// Types:
 
-typedef unsigned int  uint;
+typedef unsigned int uint;
 typedef unsigned char byte;
 
-typedef uintptr_t     addr; // an address as a numeric type
+typedef uintptr_t addr; // an address as a numeric type
 
 /** The header for a chunk. */
 typedef struct header {
-	uint status;    /**< the chunk's status -- ALLOC or FREE */
-	uint size;      /**< number of bytes, including header */
-	byte data[];    /**< the chunk's data -- not interesting to us */
+    uint status;    /**< the chunk's status -- ALLOC or FREE */
+    uint size;      /**< number of bytes, including header */
+    byte data[];    /**< the chunk's data -- not interesting to us */
 } header;
 
 /** The heap's state */
 struct heap {
-	void  *heapMem;     /**< space allocated for Heap */
-	uint   heapSize;    /**< number of bytes in heapMem */
-	void **freeList;    /**< array of pointers to free chunks */
-	uint   freeElems;   /**< number of elements in freeList[] */
-	uint   nFree;       /**< number of free chunks */
+    void *heapMem;     /**< space allocated for Heap */
+    uint heapSize;    /**< number of bytes in heapMem */
+    void **freeList;    /**< array of pointers to free chunks */
+    uint freeElems;   /**< number of elements in freeList[] */
+    uint nFree;       /**< number of free chunks */
 };
 
 
@@ -49,94 +49,116 @@ static struct heap Heap;
 
 /// Functions:
 
-static addr heapMaxAddr (void);
+static addr heapMaxAddr(void);
 
 
 /** Initialise the Heap. */
-int initHeap (int size)
-{
-	Heap.nFree = 0;
-	Heap.freeElems = 0;
+int initHeap(int size) {
+    // make sure it is a minimum of MIN_HEAP
+    if (size < MIN_HEAP) {
+        size = MIN_HEAP;
+    }
 
-	/// TODO ///
+    // round up to a multiple of 4
+    size = size + (4 - (size % 4)) % 4;
 
-	return 0; // this just keeps the compiler quiet
+    // initialise heap memory
+    Heap.heapMem = calloc(1, size);
+    if (Heap.heapMem == NULL) {
+        return -1;
+    }
+    ((header *) Heap.heapMem)->status = FREE;
+    ((header *) Heap.heapMem)->size = size;
+
+    // initialise heap wrapper
+    Heap.freeList = calloc(1, size / MIN_CHUNK);
+    if (Heap.freeList == NULL) {
+        return -1;
+    }
+
+    // sets the first item in this array to the single free-space chunk
+    Heap.freeList[0] = Heap.heapMem;
+
+    // init other variables
+    Heap.heapSize = size;
+    Heap.nFree = 0;
+    Heap.freeElems = 0;
+
+    return 0;
 }
 
 /** Release resources associated with the heap. */
-void freeHeap (void)
-{
-	free (Heap.heapMem);
-	free (Heap.freeList);
+void freeHeap(void) {
+    free(Heap.heapMem);
+    free(Heap.freeList);
 }
 
 /** Allocate a chunk of memory large enough to store `size' bytes. */
-void *myMalloc (int size)
-{
-	/// TODO ///
+void *myMalloc(int size) {
+    /// TODO ///
 
-	return NULL; // this just keeps the compiler quiet
+    return NULL; // this just keeps the compiler quiet
 }
 
 /** Deallocate a chunk of memory. */
-void myFree (void *obj)
-{
-	/// TODO ///
+void myFree(void *obj) {
+    /// TODO ///
 }
 
 /** Return the first address beyond the range of the heap. */
-static addr heapMaxAddr (void)
-{
-	return (addr) Heap.heapMem + Heap.heapSize;
+static addr heapMaxAddr(void) {
+    return (addr) Heap.heapMem + Heap.heapSize;
 }
 
 /** Convert a pointer to an offset in the heap. */
-int heapOffset (void *obj)
-{
-	addr objAddr = (addr) obj;
-	addr heapMin = (addr) Heap.heapMem;
-	addr heapMax =        heapMaxAddr ();
-	if (obj == NULL || !(heapMin <= objAddr && objAddr < heapMax))
-		return -1;
-	else
-		return (int) (objAddr - heapMin);
+int heapOffset(void *obj) {
+    addr objAddr = (addr) obj;
+    addr heapMin = (addr) Heap.heapMem;
+    addr heapMax = heapMaxAddr();
+    if (obj == NULL || !(heapMin <= objAddr && objAddr < heapMax))
+        return -1;
+    else
+        return (int) (objAddr - heapMin);
 }
 
 /** Dump the contents of the heap (for testing/debugging). */
-void dumpHeap (void)
-{
-	int onRow = 0;
+void dumpHeap(void) {
+    int onRow = 0;
 
-	// We iterate over the heap, chunk by chunk; we assume that the
-	// first chunk is at the first location in the heap, and move along
-	// by the size the chunk claims to be.
-	addr curr = (addr) Heap.heapMem;
-	while (curr < heapMaxAddr ()) {
-		header *chunk = (header *) curr;
+    // We iterate over the heap, chunk by chunk; we assume that the
+    // first chunk is at the first location in the heap, and move along
+    // by the size the chunk claims to be.
+    addr curr = (addr) Heap.heapMem;
+    while (curr < heapMaxAddr()) {
+        header *chunk = (header *) curr;
 
-		char stat;
-		switch (chunk->status) {
-		case FREE:  stat = 'F'; break;
-		case ALLOC: stat = 'A'; break;
-		default:
-			fprintf (
-				stderr,
-				"myHeap: corrupted heap: chunk status %08x\n",
-				chunk->status
-			);
-			exit (1);
-		}
+        char stat;
+        switch (chunk->status) {
+            case FREE:
+                stat = 'F';
+                break;
+            case ALLOC:
+                stat = 'A';
+                break;
+            default:
+                fprintf(
+                        stderr,
+                        "myHeap: corrupted heap: chunk status %08x\n",
+                        chunk->status
+                );
+                exit(1);
+        }
 
-		printf (
-			"+%05d (%c,%5d)%c",
-			heapOffset ((void *) curr),
-			stat, chunk->size,
-			(++onRow % 5 == 0) ? '\n' : ' '
-		);
+        printf(
+                "+%05d (%c,%5d)%c",
+                heapOffset((void *) curr),
+                stat, chunk->size,
+                (++onRow % 5 == 0) ? '\n' : ' '
+        );
 
-		curr += chunk->size;
-	}
+        curr += chunk->size;
+    }
 
-	if (onRow % 5 > 0)
-		printf ("\n");
+    if (onRow % 5 > 0)
+        printf("\n");
 }
